a0577cfc3e83ce0d06c0bd78a7f9b6d6a6b70e44
rmoj <rmoj99@gmail.com>
2018-06-30 15:52:30 -0400

    Structure code into functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+'use strict';
+
+$(document).ready(function() {
+  var characters = [
+    //Name, Image source, Health Points, Attack Power, Counter Attack Power
+    ['Darth Sidius', 'assets/images/sidious.jpg', 100, 10, 25],
+    ['Darth Vader', 'assets/images/vader.jpg', 150, 15, 20],
+    ['Count Dooku', 'assets/images/countdooku.jpg', 200, 20, 15],
+    ['Darth Maul', 'assets/images/maul.jpg', 250, 25, 10]
+  ];
+
+  //state variables
+  var hasDefender = 0;
+  var playerHP = 0;
+  var playerAP = 0;
+  var defenderHP = 0;
+  var defenderCAP = 0;
+
+  function initializeGame() {
+    //initialize state variables
+    var hasDefender = 0;
+    var playerHP = 0;
+    var playerAP = 0;
+    var defenderHP = 0;
+    var defenderCAP = 0;
+
+    //Move characters to starting area
+    $('.char').appendTo('#start-here');
+  }
+
+  function choosePlayer() {}
+
+  initializeGame();
+  choosePlayer();
+  chooseEnemy();
+  fight();
+  gameEnd();
+}); // EOF
